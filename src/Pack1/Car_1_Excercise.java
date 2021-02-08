37f84003f62ba98e457551ed2cec02b230f068ca
Krystian Wiewior <krystian.wiewior@gmail.com>
2021-02-08 21:53:34 +0100

    Excercise 1,2

--- src/Pack1/Car_1_Excercise.java
+++ src/Pack1/Car_1_Excercise.java
@@ -2,20 +2,37 @@ package Pack1;
 //1. Znajdz w swiecie rzeczywistym obiekt ktory ma rozne stany
 // oraz zachowania i odwzoruj go piszac klase reprezentujaca dany obiekt.
 
+class Car_1_Excercise {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Audi", "A3", "Strong", 4, 280, "Very goode fo young people");
+        Car car2 = new Car("Toyota", "Avensis", "Strong", 4, 230, "Cheap");
+
+        car1.inceasingSpeed(30, 2);
+        System.out.println("");
+
+        car2.toot();
+
+    }
+
+
+}
+
 
 class Car {
     String company;
-    String name;
+    String model;
     String gearbox;
     int wheelsQuantity;
     int maxSpeed;
     String additional;
 
 
-    private Car(String company, String name, String gearbox, int wheelsQuantity, int maxSpeed, String additional) {
+    Car(String company, String model, String gearbox, int wheelsQuantity, int maxSpeed, String additional) {
 
         this.company = company;
-        this.name = name;
+        this.model = model;
         this.gearbox = gearbox;
         this.wheelsQuantity = wheelsQuantity;
         this.maxSpeed = maxSpeed;
@@ -24,15 +41,18 @@ class Car {
     }
 
     void toot() {
-        System.out.println("Car makes a noise.. toot;)");
+        System.out.println("Car "+ company + " "+ model + " makes a noise.. toot;)");
     }
 
     void inceasingSpeed(int speed, int increment) {
 
         for(int i = 0; i < maxSpeed; i++) {
-        speed =+ increment;
+        speed += increment;
+        if(speed == maxSpeed){
+            break;
+            }
         }
-        System.out.println("This is your maximum speed");
+        System.out.println("Your speed was increased to " + speed );
     }
 
     void decreasingSpeed(int speed, int decrement) {
